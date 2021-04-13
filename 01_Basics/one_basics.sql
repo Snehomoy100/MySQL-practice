a457881d06ebb07aa764208dd72eded1dae1e81e
snehomoy100 <snehomoy100@gmail.com>
2021-04-13 22:08:20 +0530

    01_Basics file updated

---
+++ 01_Basics/one_basics.sql
@@ -0,0 +1,19 @@
+-- CREATE DATABASE photo_store;
+
+-- SHOW DATABASES;
+
+-- SELECT DATABASE();
+
+-- USE photo_store;
+
+-- SELECT DATABASE();
+
+-- CREATE TABLE cameras(
+--     model_name VARCHAR(30),
+--     quantity INT
+-- );
+
+DESC cameras;
+
+
+-- TODO: photo_store database creation & cameras table addition done
