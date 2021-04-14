b5be0bd10d18a9214e3ae9d24ace56cc35835b54
snehomoy100 <snehomoy100@gmail.com>
2021-04-14 21:39:47 +0530

    one_basics.sql file updated

--- 01_Basics/one_basics.sql
+++ 01_Basics/one_basics.sql
@@ -1,3 +1,5 @@
+-- TODO: photo_store database creation & cameras table addition done
+
 -- CREATE DATABASE photo_store;
 
 -- SHOW DATABASES;
@@ -16,9 +18,31 @@
 -- DESC cameras;
 
 
--- TODO: photo_store database creation & cameras table addition done
 
 
-DROP TABLE cameras;
+
+-- TODO: here we have deleted the cameras table 
+
+USE photo_store;
+
+-- DROP TABLE cameras;
+
+-- SHOW TABLES;
+
+
+
+
+
+-- TODO: now have to create another table named canon_cameras
+
+-- CREATE TABLE canon_cameras(
+--     model_name VARCHAR(30),
+--     quantity INT
+-- );
+
+
+-- DESC canon_cameras;
+
 
 SHOW TABLES;
+
